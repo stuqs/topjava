180f7a6877d6a685a5650836dbc682a4b8a812c2
Kozarezniuk Aleksandr <stuqs@ukr.net>
2017-03-30 09:10:49 +0300

    2 2 HW1 optional

---
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 30.3.2017
+ * Made by stuqs
+ */
+
+package ru.javawebinar.topjava.repository;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
+
+/**
+ * GKislin
+ * 06.03.2015.
+ */
+public interface MealRepository {
+    Meal save(Meal Meal);
+
+    void delete(int id);
+
+    Meal get(int id);
+
+    Collection<Meal> getAll();
+}
