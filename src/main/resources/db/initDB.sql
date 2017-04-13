d4dd3ef204ccf188e0e928c461d814fa9899851e
Kozarezniuk Aleksandr <stuqs@ukr.net>
2017-04-13 22:15:15 +0300

    HW03 v1

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,25 +1,36 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START 100000;
 
-CREATE TABLE users
-(
-  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name       VARCHAR NOT NULL,
-  email      VARCHAR NOT NULL,
-  password   VARCHAR NOT NULL,
-  registered TIMESTAMP DEFAULT now(),
-  enabled    BOOL DEFAULT TRUE,
-  calories_per_day INTEGER DEFAULT 2000 NOT NULL
-);
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE TABLE users (
+  id               INT UNSIGNED NOT NULL PRIMARY KEY     AUTO_INCREMENT,
+  name             VARCHAR(100) NOT NULL,
+  email            VARCHAR(45)  NOT NULL,
+  password         VARCHAR(45)  NOT NULL,
+  registered       TIMESTAMP    NOT NULL                 DEFAULT CURRENT_TIMESTAMP,
+  enabled          TINYINT      NOT NULL                 DEFAULT 1,
+  calories_per_day INT          NOT NULL                 DEFAULT 2000
+)
+  AUTO_INCREMENT = 100000;
 
-CREATE TABLE user_roles
-(
-  user_id INTEGER NOT NULL,
-  role    VARCHAR,
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
+
+CREATE TABLE user_roles (
+  user_id INT UNSIGNED NOT NULL,
+  role    VARCHAR(45),
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE meals (
+  id          INT UNSIGNED NOT NULL PRIMARY KEY     AUTO_INCREMENT,
+  date_time   TIMESTAMP    NOT NULL,
+  description VARCHAR(255),
+  calories    INT UNSIGNED,
+  user_id     INT UNSIGNED NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
 );
